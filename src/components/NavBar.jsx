1c475f1a1a90cd441f8142df9708770b1001ce2a
Nacho <ignacio_coletta@hotmail.com>
2024-08-18 10:55:16 -0300

    fix: importaciones no usadas

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import CartWidget from './CartWidget';
-import logo from '../assets/images/logo.png';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
